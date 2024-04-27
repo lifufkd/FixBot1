01a98e25d4c6549eef872a4cdd519436c4c2a3d1
lilfuz <lilfuz@gmail.com>
2024-04-27 20:30:10 +0300

    fix

--- main.py
+++ main.py
@@ -35,31 +35,16 @@ def main():
     def txt_msg(message):
         user_id = message.chat.id
         user_input = message.text
-        code = temp_user_data.temp_data(user_id)[user_id][0]
+        buttons = Bot_inline_btns()
         delete_msg(user_id)
         if user_input == '✅написать историю✅':
             bot.delete_message(user_id, message.id)
             message_id = bot.send_message(message.chat.id, "Пожалуйста, напишите свою историю:").message_id
             temp_user_data.temp_data(user_id)[user_id][1] = message_id
         else:
+            bot.forward_message(config.get_config()['group_id'], user_id, message.id)
             bot.delete_message(user_id, message.id)
-            if message.text:
-                bot.send_message(config.get_config()['group_id'], message.text)
-            # Если сообщение содержит фотографию, отправляем её
-            if message.photo:
-                # Получаем информацию о фотографии
-                photo = message.photo[-1]
-                # Отправляем фотографию в ваш чат
-                bot.send_photo(config.get_config()['group_id'], photo.file_id)
-            # Если сообщение содержит видео, отправляем его
-            if message.video:
-                # Получаем информацию о видео
-                video = message.video
-                # Отправляем видео в ваш чат
-                bot.send_video(config.get_config()['group_id'], video.file_id)
-            message_id = bot.send_message(user_id, '✅история отправлена на проверку✅').message_id
-            bot.delete_message(user_id, message_id)
-            message_id = bot.send_message(message.chat.id, "Пожалуйста, напишите свою историю:").message_id
+            message_id = bot.send_message(user_id, '✅история отправлена на проверку✅', reply_markup=buttons.start_buttons()).message_id
             temp_user_data.temp_data(user_id)[user_id][1] = message_id
 
     bot.polling(none_stop=True)
