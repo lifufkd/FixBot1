624d2eff256f310030efa526a07efb2f94767384
lilfuz <lilfuz@gmail.com>
2024-04-27 20:05:33 +0300

    Initial commit

---
+++ backend.py
@@ -0,0 +1,97 @@
+
+import os
+import time
+import json
+import csv
+
+class TempUserData:
+    def __init__(self):
+        super(TempUserData, self).__init__()
+        self.__user_data = {}
+
+    def temp_data(self, user_id):
+        if user_id not in self.__user_data.keys():
+            self.__user_data.update({user_id: [None, None, None]})
+        return self.__user_data
+
+
+class DbAct:
+    def __init__(self, db, config, path_xlsx):
+        super(DbAct, self).__init__()
+        self.__db = db
+        self.__config = config
+        self.__fields = ['Имя', 'Фамилия', 'Никнейм', 'Номер телефона']
+        self.__dump_path_xlsx = path_xlsx
+
+    def add_user(self, user_id, first_name, last_name, nick_name):
+        if not self.user_is_existed(user_id):
+            if user_id in self.__config.get_config()['admins']:
+                is_admin = True
+            else:
+                is_admin = False
+            self.__db.db_write(
+                'INSERT INTO users (user_id, first_name, last_name, nick_name, is_admin, exp_date, notes_count, subscription_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                (user_id, first_name, last_name, nick_name, is_admin, time.time()+2592000, 30, 3))
+
+    def user_is_existed(self, user_id):
+        data = self.__db.db_read('SELECT count(*) FROM users WHERE user_id = ?', (user_id,))
+        if len(data) > 0:
+            if data[0][0] > 0:
+                status = True
+            else:
+                status = False
+            return status
+
+    def user_is_admin(self, user_id):
+        data = self.__db.db_read('SELECT is_admin FROM users WHERE user_id = ?', (user_id,))
+        if len(data) > 0:
+            if data[0][0] == 1:
+                status = True
+            else:
+                status = False
+            return status
+
+    def check_subscription(self, user_id):
+        data = self.__db.db_read('SELECT exp_date, notes_count, subscription_type FROM users WHERE user_id = ?', (user_id,))
+        if len(data) > 0:
+            if data[0][0] > time.time() and data[0][1] > 0:
+                if data[0][2] == 3 and data[0][1] > 0:
+                    return True
+                elif data[0][2] != 3:
+                    return True
+            elif self.user_is_admin(user_id):
+                return True
+
+    def get_eol(self, user_id):
+        return self.__db.db_read('SELECT exp_date, notes_count, subscription_type FROM users WHERE user_id = ?', (user_id, ))[0]
+
+    def update_subscription_time(self, user_nick, times):
+        self.__db.db_write('UPDATE users SET exp_date = ? WHERE nick_name = ?', (times, user_nick))
+
+    def update_subscription_notes(self, user_nick, notes_count):
+        self.__db.db_write('UPDATE users SET notes_count = ? WHERE nick_name = ?', (notes_count, user_nick))
+
+    def give_subscription(self, nick_name, date, sub_type, notes=0):
+        if notes == 0:
+            self.__db.db_write('UPDATE users SET exp_date = ?, subscription_type = ? WHERE nick_name = ?', (date, sub_type, nick_name))
+        else:
+            self.__db.db_write('UPDATE users SET exp_date = ?, subscription_type = ?, notes_count = ? WHERE nick_name = ?',
+                               (date, sub_type, notes, nick_name))
+
+    def give_subscription_ud(self, user_id, date, sub_type, notes=0):
+        if notes == 0:
+            self.__db.db_write('UPDATE users SET exp_date = ?, subscription_type = ? WHERE user_id = ?', (date, sub_type, user_id))
+        else:
+            self.__db.db_write('UPDATE users SET exp_date = ?, subscription_type = ?, notes_count = ? WHERE user_id = ?',
+                               (date, sub_type, notes, user_id))
+
+    def update_notion_token(self, notion_token, user_id):
+        self.__db.db_write('UPDATE users SET notion_token = ? WHERE user_id = ?', (notion_token, user_id))
+
+    def update_notion_db(self, user_id, data):
+        self.__db.db_write('UPDATE users SET db_info = ? WHERE user_id = ?', (json.dumps(data), user_id))
+
+    def get_notion_db(self, user_id):
+        data = self.__db.db_read('SELECT db_info FROM users WHERE user_id = ?', (user_id, ))
+        if len(data) > 0:
+            return json.loads(data[0][0])
---
+++ frontend.py
@@ -0,0 +1,48 @@
+import telebot
+from telebot import types
+
+class Bot_inline_btns:
+    def __init__(self):
+        super(Bot_inline_btns, self).__init__()
+        self.__markup = types.InlineKeyboardMarkup(row_width=1)
+
+    def start_buttons(self):
+        first = types.InlineKeyboardButton('Авторизоваться', url="https://api.notion.com/v1/oauth/authorize?client_id=615dba0d-2dcd-4e9d-b6c5-d543f495984d&response_type=code&owner=user&redirect_uri=https://t.me/wfwfwegwvwefbot/?start=")
+        second = types.InlineKeyboardButton('Готово', callback_data='done')
+        t = types.InlineKeyboardButton('Подписка', callback_data='sub')
+        self.__markup.add(first, second, t)
+        return self.__markup
+    def admin_btns(self):
+        l = types.InlineKeyboardButton('Выдать лимит', callback_data='givelimit')
+        sub = types.InlineKeyboardButton('Выдать подписку', callback_data='givesub')
+        self.__markup.add(l, sub)
+        return self.__markup
+
+    def actions_btns(self):
+        d1 = types.InlineKeyboardButton('Время истечения подписки', callback_data='restrict0')
+        d2 = types.InlineKeyboardButton('Количество заметок', callback_data='restrict1')
+        self.__markup.add(d1, d2)
+        return self.__markup
+
+    def payment_btn(self):
+        f = types.InlineKeyboardButton('1 месяц - 299₽', callback_data='subscription0')
+        s = types.InlineKeyboardButton('6 месяцев - 1399₽', callback_data='subscription1')
+        t = types.InlineKeyboardButton('12 месяцев - 2599₽', callback_data='subscription2')
+        z = types.InlineKeyboardButton('30 запросов на месяц - 99₽', callback_data='subscription3')
+        self.__markup.add(f, s, t, z)
+        return self.__markup
+
+    def cnt_btn(self):
+        f = types.InlineKeyboardButton('1 месяц', callback_data='cnt0')
+        s = types.InlineKeyboardButton('6 месяцев', callback_data='cnt1')
+        t = types.InlineKeyboardButton('12 месяцев', callback_data='cnt2')
+        z = types.InlineKeyboardButton('30 запросов на месяц', callback_data='cnt3')
+        self.__markup.add(f, s, t, z)
+        return self.__markup
+
+    def notion_db_btns(self, names):
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        for index, i in enumerate(names):
+            z = types.InlineKeyboardButton(i, callback_data=f'notions_dbs{index}')
+            markup.add(z)
+        return markup
